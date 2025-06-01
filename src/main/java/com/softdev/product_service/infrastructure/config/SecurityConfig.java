0706d759780542c202e25231d337e2085e28067f
MarHeavenn <bluestaromc@gmail.com>
2025-06-01 15:00:18 -0500

    add: seguridad endpoints

--- src/main/java/com/softdev/product_service/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/softdev/product_service/infrastructure/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
-                        .requestMatchers("/ordenes/**").hasRole("CLIENTE")
+                        .requestMatchers("/producto/**").hasAnyRole("ADMINISTRADOR","REPARTIDOR","CLIENTE")
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
